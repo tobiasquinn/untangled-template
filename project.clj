b7562c33a57540dbf4fe2fc949027520c9e2a7a5
Anthony D'Ambrosio <adambrosio@thenavisway.com>
2016-02-18 16:38:14 -0800

    0.3.0

--- project.clj
+++ project.clj
@@ -1,7 +1,15 @@
-(defproject untangled/lein-template "0.2.1"
+(defproject untangled/lein-template "0.3.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.7.0"]]
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :eval-in-leiningen true)
+  :eval-in-leiningen true
+
+  :deploy-repositories [["releases" {:id            "central"
+                                     :url           "https://artifacts.buehner-fry.com/artifactory/navis-maven-release"
+                                     :snapshots     false
+                                     :sign-releases false}]
+                        ["snapshots" {:id            "snapshots"
+                                      :url           "https://artifacts.buehner-fry.com/artifactory/navis-maven-snapshots"
+                                      :sign-releases false}]])
