43e5845b11892989897f8ffcbadc63ea55d05f66
Anthony D'Ambrosio <adambrosio@thenavisway.com>
2016-02-19 16:36:41 -0800

    0.3.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject untangled/lein-template "0.3.4"
+(defproject untangled/lein-template "0.3.4-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.7.0"]]
