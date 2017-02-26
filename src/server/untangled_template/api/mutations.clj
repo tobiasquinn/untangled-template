ffeb74c2568ca7b32e0ddee2ba37e38952c3839a
Tony Kay <twkay@thenavisway.com>
2017-02-25 16:28:23 -0800

    added parser injection example

--- src/server/untangled_template/api/mutations.clj
+++ src/server/untangled_template/api/mutations.clj
@@ -15,5 +15,7 @@
              {:uid     42
               :tempids {uid 42}})})
 
-(defmethod apimutate 'login/logout [env k {:keys [u p uid]}]
+(defmethod apimutate 'login/logout [{:keys [sample-component] :as env} k {:keys [u p uid]}]
+  ; Demo that the injected component appears in env
+  (timbre/info "Sample component data: " (:stuff sample-component))
   {:action (fn [] (reset! logged-in? false))})
